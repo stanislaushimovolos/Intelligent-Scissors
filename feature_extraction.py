46aab55ff06ef6d92bbd591cf5f98426dedafd06
stanislaushimovolos <shimovolos.sa@phystech.edu>
2020-04-23 17:43:23 +0300

    added laplacian costs

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,21 +1,21 @@
 import numpy as np
 from dijkstar import Graph
 from itertools import product
-from skimage.filters import laplace, sobel_h, sobel_v
+from skimage.filters import gaussian, laplace, sobel_h, sobel_v
 
 from utils import unfold
 
 shifts = [(-1, 1), (-1, 0), (-1, -1), (0, 1), (0, -1), (1, -1), (1, 0), (1, 1)]
 
 default_weights = {
-    'laplace': 0,
-    'direction': 0.9,
+    'laplace': 0.2,
+    'direction': 0.7,
     'magnitude': 0.1
 }
 
 
 class StaticFeatureExtractor:
-    def __init__(self, gaussian_std=2, laplace_kernel_size=5, weights=None, connect_area=3):
+    def __init__(self, gaussian_std=2, laplace_filter_size=5, weights=None, connected_area=3):
         if weights is None:
             weights = default_weights
 
@@ -24,25 +24,40 @@ class StaticFeatureExtractor:
         self.direction_w = weights['direction']
 
         self.gaussian_std = gaussian_std
-        self.laplace_kernel_size = laplace_kernel_size
-        self.filter_size = np.array([connect_area, connect_area])
+        self.laplace_filter_size = laplace_filter_size
+        self.filter_size = np.array([connected_area, connected_area])
 
     def get_total_link_costs(self, image):
-        # l_cost = self.get_laplace_cost(image, self.laplace_kernel_size, self.gaussian_std)
-        # l_cost = unfold(l_cost, self.filter_size)
+        l_cost = self.get_laplace_cost(image, self.laplace_filter_size, self.gaussian_std)
+        l_cost = unfold(l_cost, self.filter_size)
 
         g_cost = self.get_magnitude_cost(image)
         g_cost = unfold(g_cost, self.filter_size)
 
         d_cost = self.get_direction_cost(image)
-        total_cost = self.magnitude_w * g_cost + self.direction_w * d_cost
+        total_cost = self.laplace_w * l_cost + self.magnitude_w * g_cost + self.direction_w * d_cost
         return total_cost
 
     @staticmethod
-    def get_laplace_cost(img, kernel_size, std):
-        laplace_map = laplace(img, ksize=kernel_size)
+    def get_laplace_cost(image, filter_size, std):
+        output = np.ones_like(image)
+        blurred = gaussian(image, std)
+        laplace_map = laplace(blurred, ksize=filter_size)
 
-        return np.abs(laplace_map) > 0.1
+        unfolded_laplace = unfold(
+            np.expand_dims(laplace_map, 0),
+            np.array([filter_size, filter_size])
+        )
+        unfolded_laplace = np.reshape(
+            unfolded_laplace, (filter_size * filter_size,) + laplace_map.shape
+        )
+        n_positive = np.sum(unfolded_laplace >= 0, axis=0)
+        n_negative = np.sum(unfolded_laplace < 0, axis=0)
+
+        # TODO check minimum value
+        zero_crossing_mask = ((n_negative > 0) & (n_positive > 0))
+        output[zero_crossing_mask] = 0
+        return np.expand_dims(output, 0)
 
     @staticmethod
     def get_magnitude_cost(image):
@@ -87,6 +102,11 @@ class StaticFeatureExtractor:
         distant_feats = sign_mask * np.einsum('i..., i...', spatial_feats, unfolded_grads)
         total_cost = 2 / (3 * np.pi) * (np.arccos(local_feats) + np.arcsin(distant_feats))
         return total_cost
+
+
+class PixelEdgesWrapper:
+    def __init__(self, pixel_neighbours):
+        self.neighbours = pixel_neighbours
 
 
 class IndexBasedGraphWrapper:
