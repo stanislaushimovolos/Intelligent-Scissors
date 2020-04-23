887dac9fc23ff208c99eb99bf755bae75a0609b9
stanislaushimovolos <shimovolos.sa@phystech.edu>
2020-04-24 01:00:56 +0300

    added simple polygon model

--- gui.py
+++ gui.py
@@ -33,35 +33,49 @@ class Controller:
 
 
 class View:
-    def __init__(self, model, filename, fill_color="red", radius=5):
+    def __init__(self, model, image, fill_color="red", radius=3):
         self.model = model
+
         self.radius = radius
         self.fill_color = fill_color
 
-        self.image = ImageTk.PhotoImage(Image.open(filename))
+        self.image = ImageTk.PhotoImage(image)
         self.canvas.create_image(0, 0, image=self.image, anchor=NW)
+
+    def update(self):
+        self.canvas.delete("all")
+        # print(self.canvas['width'])
+        self.canvas.create_image(0, 0, image=self.image, anchor=NW)
+        points = self.model.points
+
+        for previous, current in zip(points, points[1:]):
+            self.canvas.create_line(*previous, *current)
+
+        for p in points:
+            self.canvas.create_oval(
+                p[0] - self.radius, p[1] - self.radius,
+                p[0] + self.radius, p[1] + self.radius,
+                fill=self.fill_color
+            )
 
     @property
     def canvas(self):
         return self.model.canvas
 
-    def update(self):
-        self.canvas.delete("all")
-        self.canvas.create_image(0, 0, image=self.image, anchor=NW)
-        points = self.model.points
-        for p in points:
-            self.canvas.create_oval(
-                p[0], p[1], p[0] + self.radius, p[1] + self.radius, fill=self.fill_color
-            )
 
+image = Image.open('test_img.jpg')
+width, height = image.size
 
 root = Tk()
-stage = Canvas(root, bg="black", height=250, width=300)
-stage.pack(expand=YES, fill=BOTH)
 
+stage = Canvas(root, bg="black", height=height, width=width)
 m = Model(stage)
+v = View(m, image=image)
 m.add_view(v)
 
 c = Controller(m)
 stage.bind('<Button-1>', c.on_click)
+stage.pack(expand=YES, fill=BOTH)
+
+root.resizable(False, False)
 root.mainloop()
