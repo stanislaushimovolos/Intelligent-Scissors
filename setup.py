923d9d2a35a480c78191d7c391811a9c1c53abc2
stanislaushimovolos <shimovolos.sa@phystech.edu>
2020-04-24 15:37:21 +0300

    small changes

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 import setuptools
+from scissors import __version__
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 with open('requirements.txt', encoding='utf-8') as file:
     requirements = file.read().splitlines()
 
 setuptools.setup(
-    name="intelligent-scissors",
-    version="0.0.1",
+    name='intelligent-scissors',
+    version=__version__,
     author="Stanislav Shimovolos",
     author_email='shimovolos.sa@phystech.edu',
     description="Intelligent Scissors tool",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     license='MIT',
-    url="https://github.com/stanislaushimovolos/Intelligent-Scissors",
+    url='https://github.com/stanislaushimovolos/Intelligent-Scissors',
+    download_url='https://github.com/stanislaushimovolos/Intelligent-Scissors/v%s.tar.gz' % __version__,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -23,5 +25,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=requirements,
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
